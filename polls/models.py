aa25c02332223d414745f9464067e2bea2206a31
zhumaobi <zhumaobi@qq.com>
2017-08-01 13:04:55 +0800

    create my first Django webapplication in Part5(TestViewFunction)

--- polls/models.py
+++ polls/models.py
@@ -9,7 +9,8 @@ class Question(models.Model):
 	def __str__(self):
 		return self.question_text
 	def was_published_recently(self):
-		return self.pub_date >= (timezone.now() - datetime.timedelta(days=1))
+		now = timezone.now()
+		return now >= self.pub_date >= (now - datetime.timedelta(days=1))
 	was_published_recently.admin_order_field = 'pub_date'
 	was_published_recently.boolean = True
 	was_published_recently.short_description = 'Published recently?'
